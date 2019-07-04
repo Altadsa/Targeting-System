2afa339ce99d11d20d2d665ce5b64de5fd272637
Adam Coyle <adam.coyle@fhnw.ch>
2019-07-04 12:05:36 +0200

    Add switch to move from first person to 3rd person.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -17,16 +17,43 @@ public class CameraController : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(1))
+        {
+            EnableFirstPerson();
+        }
+
+        if (_fCam.enabled && Input.GetKeyDown(KeyCode.R))
+        {
+            DisableFirstPerson();
+        }
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             _zCam.enabled = true;
             _freeCam.enabled = false;
+            _fCam.enabled = false;
         }
         else
         {
-            _zCam.enabled = false;
-            _freeCam.enabled = true;
+            if (!_fCam.enabled)
+            {
+                _zCam.enabled = false;
+                _freeCam.enabled = true;
+            }
         }
     }
+
+    private void EnableFirstPerson()
+    {
+        _fCam.enabled = true;
+        _freeCam.enabled = false;
+    }
+
+    private void DisableFirstPerson()
+    {
+        _fCam.enabled = false;
+        _freeCam.enabled = true;
+    }
+    
 
 }
