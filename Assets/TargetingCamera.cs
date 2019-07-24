0a6f6d9491b3bd8461e8569e901c406e2df0240e
Adam Coyle <adam.coyle@fhnw.ch>
2019-07-24 15:07:55 +0200

    Targeting Camera changes its offset in proportion to the distance between targets.

--- Assets/TargetingCamera.cs
+++ Assets/TargetingCamera.cs
@@ -6,33 +6,37 @@ public class TargetingCamera : PlayerCamera
 {
     [Header ("Targets")]
     // Player and Target transforms
-    public Transform Player;
+    [SerializeField] Transform Player;
     public Transform Target;
 
     [Header("Variables")]
-    public Vector3 PositionOffset;
     public Vector3 TargetingArea;
     public float SpeedOffset = 5;
 
     [Tooltip("The Targetable GameObjects Layer")]
     public LayerMask LayerMask;
 
+    //Minimum Distance behind the Player that the Camera should stay.
+    private const float MIN_TARGETING_DISTANCE = 3f;
+
+    float DistanceOffset => _midpointVector.magnitude < 5 ? 0 : MAX_POS_OFFSET * _midpointVector.magnitude / 10;
+
+    private const float MAX_POS_OFFSET = 2;
     //The angle between the midpoint of the Targets and the Camera
-    private const float OFFSET = 45;
+    private const float ANGLE_OFFSET = 45;
 
     // Directional Vector between the Player and Target
     private Vector3 _midpointVector;
     //World Coordinates of the midpoint between Player and Target
     private Vector3 _targetMidpoint;
-    //Distance behind the Player that the Camera should stay.
-    public float TargetingDistance;
+
     //Boolean to determine whether Camera should orient itself to Player on left or right
     private bool _directionToRotate;
 
     private Vector3 _fixedForward;
 
     //Gets the Fixed position behind the Player
-    private Vector3 FixedFollow => Player.position - _fixedForward * TargetingDistance * 2 + PositionOffset;
+    private Vector3 FixedFollow => Player.position - _fixedForward * MIN_TARGETING_DISTANCE * 2 + Vector3.up;
 
     //Enable Player ability to move if coming from First Person
     private void OnEnable()
@@ -50,6 +54,7 @@ public class TargetingCamera : PlayerCamera
     {
         if (Target)
         {
+            Debug.Log(_midpointVector.magnitude);
             _midpointVector = Target.position - Player.position;
             _targetMidpoint = Player.position + 0.5f * _midpointVector;
             var camMidAngle = Vector3.SignedAngle((_midpointVector - transform.position).normalized, _midpointVector.normalized, Vector3.up);
@@ -96,9 +101,12 @@ public class TargetingCamera : PlayerCamera
 
     private Vector3 TargetingPosition()
     {
-        var rot = !_directionToRotate ? OFFSET : -OFFSET;
+        var rot = !_directionToRotate ? ANGLE_OFFSET : -ANGLE_OFFSET;
         var rotFor = Quaternion.AngleAxis(rot, Vector3.up) * -Vector3.Scale(_midpointVector, new Vector3(1,0,1)).normalized;
-        return Player.position + rotFor * TargetingDistance + PositionOffset;
+        var offset = DistanceOffset;
+        var heightOffset = new Vector3(0, 1 + offset, 0);
+        var distanceOffset = MIN_TARGETING_DISTANCE + offset;
+        return Player.position + rotFor * distanceOffset + heightOffset;
     }
 
     private Transform GetTarget()
